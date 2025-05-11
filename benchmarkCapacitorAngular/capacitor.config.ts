8afd0f42c6a9132c75e26d6db2f71844dce70e89
nitsugua <augustinmichiels@gmail.com>
2025-05-12 00:22:05 +0200

    ready for testing

--- benchmarkCapacitorAngular/capacitor.config.ts
+++ benchmarkCapacitorAngular/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.benchmark',
   appName: 'benchmark-capacitor-angular',
-  webDir: 'dist/benchmark-capacitor-angular/browser/',
+  webDir: 'dist/benchmark-capacitor-angular/browser',
 };
 
 export default config;
